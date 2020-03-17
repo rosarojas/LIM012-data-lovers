83b1fe29a896cf77ed18c448259dacab4123d07b
fiorella <fiorellachilc_20@hotmail.com>
2020-03-17 12:50:28 -0500

    updates

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import {card, ordenar} from './data.js';
 import data from './data/atletas/atletas.js';
 const arrDataAtletas = data.atletas;
-const arrDisciplinas = arrDataAtletas.filter((atleta) => atleta.hasOwnProperty('disciplinas'));
-const atletas2016 = arrDisciplinas.filter((listaAtletas) => listaAtletas.disciplinas[0].año === 2016);
+const arrDisciplinas = arrDataAtletas.filter((atleta) =>
+  (atleta.hasOwnProperty('disciplinas')));
+const atletas2016 = arrDisciplinas.filter((listaAtletas) =>
+  (listaAtletas.disciplinas[0].año === 2016));
 console.log(atletas2016);
 const topAtletas = function top() {
   const topA = [];
@@ -22,18 +24,22 @@ const topAtletas = function top() {
 console.log(topAtletas);
 
 const usuarios = topAtletas.map((indice) => arrDataAtletas[indice]);
+console.log(usuarios);
 // Funcion para ordenar de A - Z
+const main = document.getElementsByTagName('main')[0];
 const ordenador = document.getElementById('ordenador');
 ordenador.addEventListener('click', () => {
   ordenar(usuarios);
-  card(usuarios);
+  main.innerHTML = card(usuarios);
 });
 // Funcion para ordenar Z - A
 // ordenInverso(usuarios, name);
 // console.log(ordenInverso);
 // Funcion mostrar data;
-card(usuarios);
-const deportesTotal = atletas2016.map((atleta) => atleta.disciplinas[0].disciplina);
+main.innerHTML = card(usuarios);
+const deportesTotal = atletas2016.map((atleta) =>
+  (atleta.disciplinas[0].disciplina));
 console.log(deportesTotal);
-const deportes = deportesTotal.filter((value, index, self) => self.indexOf(value) === index);
+const deportes = deportesTotal.filter((value, index, self) =>
+  (self.indexOf(value) === index));
 console.log(deportes);
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,31 +1,68 @@
-import {ordenar} from '../src/data.js';
+import {ordenar, card} from '../src/data.js';
 describe('ordenar', () => {
   it('is a function', () => {
     expect(typeof ordenar).toBe('function');
   });
-  it('returns `example`', () => {
+  it('returns `ordenar`', () => {
     expect(ordenar([
       {
-        'name': 'Paola Bisiani'
+        'name': 'Paola',
       },
       {
-        'name': 'Nataliya Andrivna Burdeina'
-      }])).toBe(
-        [
-          {
-            'name': 'Nataliya Andrivna Burdeina'
-          },
-          {
-            'name': 'Paola Bisiani'
-          }]
-    );
+        'name': 'Ana',
+      },
+      {
+        'name': 'Ana',
+      },
+      {
+        'name': 'Nataliya',
+      }])).toStrictEqual([
+      {
+        'name': 'Ana',
+      },
+      {
+        'name': 'Ana',
+      },
+      {
+        'name': 'Nataliya',
+      },
+      {
+        'name': 'Paola',
+      }]);
   });
 });
-// describe('anotherExample', () => {
-//   it('is a function', () => {
-//     expect(typeof anotherExample).toBe('function');
-//   });
-//   it('returns `anotherExample`', () => {
-//     expect(anotherExample()).toBe('OMG');
-//   });
-// });
+describe('card', () => {
+  it('is a function', () => {
+    expect(typeof card).toBe('function');
+  });
+  it('returns `card`', () => {
+    expect(card([
+      {
+        'name': 'Paola Bisiani',
+      },
+      {
+        'name': 'Nataliya',
+      }])).toBe('<article><img src=\"foto.png\" alt=\"foto\"> '+
+      '<div><p>name: Paola Bisiani </p></div> </article>' +
+      '<article><img src=\"foto.png\" alt=\"foto\"> '+
+      '<div><p>name: Nataliya </p></div> </article>');
+  });
+  it('returns `card`', () => {
+    expect(card([
+      {
+        'name': 'Paola Bisiani',
+      },
+      {
+        'name': 'Nataliya',
+        'pasatiempos': [
+          {
+            'correr': 'Archery Team',
+          },
+        ],
+      }])).toBe('<article><img src=\"foto.png\" alt=\"foto\"> '+
+      '<div><p>name: Paola Bisiani </p></div> </article>' +
+      '<article><img src=\"foto.png\" alt=\"foto\"> '+
+      '<div><p>name: Nataliya </p><p>correr: '+
+      'Archery Team </p></div> </article>');
+  });
+});
